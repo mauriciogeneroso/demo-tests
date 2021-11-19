cc6f4f042ded75ace190951508cb8553c419e4e5
Mauricio Generoso <mauricio.marquesgeneroso@sky.uk>
2021-11-19 10:03:12 +0000

    comment with gradle command

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// ./gradlew clean build && java -jar ./build/libs/demo-0.0.1-SNAPSHOT.jar
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
