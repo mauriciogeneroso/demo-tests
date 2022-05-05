63293aa0255de345cc83c14ce978357ec36fe530
Mauricio Generoso <mauro_generoso@hotmail.com>
2022-05-05 14:03:00 +0100

    updated command to run module via bootJar and update project name

--- main-module/src/main/java/com/example/demo/DemoApplication.java
+++ main-module/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	// ./gradlew clean build && java -jar ./build/libs/demo-0.0.1-SNAPSHOT.jar
+	// ./gradlew mM:clean mM:bootJar && java -jar ./main-module/build/libs/main-module-0.0.1-SNAPSHOT.jar
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
