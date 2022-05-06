8984cc06d4c06e3a73c9cd1e6c43e3f329644a5c
Mauricio Generoso <mauro_generoso@hotmail.com>
2022-05-06 10:44:39 +0100

    Created a library module

---
+++ demo-library/src/main/java/com/example/demo/library/model/Hello.java
@@ -0,0 +1,8 @@
+package com.example.demo.library.model;
+
+public class Hello {
+
+    public String getHelloMessage() {
+        return "Hello World!";
+    }
+}
--- main-module/src/main/java/com/example/demo/controller/HelloController.java
+++ main-module/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.library.model.Hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
 public class HelloController {
 
+    private Hello hello = new Hello();
+
+
     @GetMapping
     public String hello() {
-        return "Helloaaa";
+        return hello.getHelloMessage();
     }
 }
