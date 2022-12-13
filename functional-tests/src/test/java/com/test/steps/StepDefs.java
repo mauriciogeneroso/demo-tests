8e24871f8e7ed261b3f04e1b66261c08bbe11aa4
Mauricio Generoso <mauro_generoso@hotmail.com>
2022-12-13 07:16:00 -0300

    Include test in parallel with different steps

--- functional-tests/src/test/java/com/test/steps/StepDefs.java
+++ functional-tests/src/test/java/com/test/steps/StepDefs.java
@@ -1,10 +1,17 @@
 package com.test.steps;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.springframework.context.annotation.Scope;
 
+import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
+
+//@Scope(SCOPE_CUCUMBER_GLUE)
 public class StepDefs {
 
     private String testVariable;
+    private String scenarioName;
+    private String fileName;
 
     @Given("a step from {string} in {string} feature file")
     public void step(String scenario, String file) throws InterruptedException {
@@ -12,6 +19,22 @@ public class StepDefs {
                 Thread.currentThread().getId(), scenario, file);
         testVariable = "variable - " + scenario;
         Thread.sleep(3000);
-        System.out.println(testVariable);
+        System.out.format("Thread ID – %2d – %s from %s feature file: %s.\n",
+                Thread.currentThread().getId(), scenario, file, testVariable);
+    }
+
+    @Given("a step {string} on the file {string} is defined")
+    public void step2(String scenario, String file) {
+        this.scenarioName = scenario;
+        this.fileName = file;
+
+        System.out.format("Thread ID – %2d – Define variables for %s from %s feature file.\n",
+                Thread.currentThread().getId(), scenario, file);
+    }
+
+    @Then("print the variables for the scenario {string}")
+    public void print(String scenario) {
+        System.out.format("Thread ID – %2d – Print variables for %s: scenarioName: %s and fileName: %s.\n",
+                Thread.currentThread().getId(), scenario, scenarioName, fileName);
     }
 }
--- functional-tests/src/test/resources/features/ScenariosOutlineExample.feature
+++ functional-tests/src/test/resources/features/ScenariosOutlineExample.feature
@@ -9,3 +9,14 @@ Feature: Outlines Example Feature File
       | Scenario Outline – Row 3    |
       | Scenario Outline – Row 4    |
       | Scenario Outline – Row 5    |
+
+  Scenario Outline: Multiple Rows Parallel with different steps
+    Given a step '<scenario_outline_row_number>' on the file 'ScenarioOutlinesExample' is defined
+    Then print the variables for the scenario '<scenario_outline_row_number>'
+    Examples:
+      | scenario_outline_row_number |
+      | Scenario Outline – Row 1    |
+      | Scenario Outline – Row 2    |
+      | Scenario Outline – Row 3    |
+      | Scenario Outline – Row 4    |
+      | Scenario Outline – Row 5    |
